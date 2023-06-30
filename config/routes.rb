ee69fae15055bb7dde2cd6f8a4696108580a155c
shinke <sinke.niinomi0506@icloud.com>
2023-06-30 11:05:37 +0900

    “custom_sessionsコントローラーのnew,createアクションへのルーチングを設定しました。”

--- config/routes.rb
+++ config/routes.rb
@@ -4,5 +4,7 @@ Rails.application.routes.draw do
   devise_scope :user do
     get 'users/registration', to: 'custom_registrations#new', as: :new_user_custom_registration
     post 'users/registration', to: 'custom_registrations#create', as: :user_custom_registration
+    get 'users/session', to: 'custom_sessions#new', as: :new_user_custom_session
+    post 'users/session', to: 'custom_sessions#create', as: :user_custom_session
   end
 end
