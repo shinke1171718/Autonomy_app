c03f343d540f4f43d87ba94117e7039d28142a73
shinke <sinke.niinomi0506@icloud.com>
2023-07-01 14:32:10 +0900

    “signinのコントローラーを実装しました”

--- app/controllers/custom_sessions_controller.rb
+++ app/controllers/custom_sessions_controller.rb
@@ -1,4 +1,38 @@
 class CustomSessionsController < ApplicationController
+  include Devise::Controllers::Helpers
+
   def new
+  end
+
+  def create
+    #userのidとパスワードを格納する
+    user = User.find_by(name: params[:name])
+    #userとパスワードが一致するか確認
+    if user && user.valid_password?(params[:password])
+      #セッションIDを払い出す
+      session[:user_id] = user.id
+      #ログインしましたとアナウンス
+      flash[:notice] = "ログインしました"
+      #もし一致する場合にはroot_pathへ移動
+      redirect_to new_user_custom_session_path
+    else
+      #ログインできませんでしたとアナウンス
+      flash[:notice] = "ユーザー名かパスワードのどちらかが違います。"
+      #合わない場合にはsessions#newへ戻る
+      render 'new'
+    end
+  end
+
+  def destroy
+    binding.pry
+    #セッションIDを処分
+    session[:user_id] = nil
+    #ログでsessions_idがnilになったか確認するためにコード
+    Rails.logger.debug "session[:user_id] = #{session[:user_id]}"
+    #セッションの完全な処理
+    reset_session
+    #sessions#newへ戻る
+    flash[:notice] = "ログアウトしました。"
+    redirect_to new_user_custom_session_path
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,10 @@
 Rails.application.routes.draw do
-  get 'custom_sessions/new'
   devise_for :users
   devise_scope :user do
     get 'users/registration', to: 'custom_registrations#new', as: :new_user_custom_registration
     post 'users/registration', to: 'custom_registrations#create', as: :user_custom_registration
     get 'users/session', to: 'custom_sessions#new', as: :new_user_custom_session
     post 'users/session', to: 'custom_sessions#create', as: :user_custom_session
+    get 'users/sessions', to: 'custom_sessions#destroy', as: :destroy_user_custom_session
   end
 end
