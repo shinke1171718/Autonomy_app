b0c90c8c7a16b9213f580ff813ca08dd0c2669ff
shinke <sinke.niinomi0506@icloud.com>
2023-07-09 14:40:38 +0900

    “メニューの一覧をレスポンシブ対応（ハンバーガーメニュー）に修正しました。”

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -1,3 +1,16 @@
 // Configure your import map in config/importmap.rb. Read more: https://github.com/rails/importmap-rails
 import "@hotwired/turbo-rails"
 import "controllers"
+
+
+let content = document.querySelector(".mobile-menu-content");
+let btn = document.querySelector(".mobile-menu-botton");
+let mask = document.querySelector("#mask");
+
+btn.onclick = () => {
+  content.classList.toggle("open")
+}
+
+mask.onclick = () => {
+  content.classList.toggle("open")
+}
