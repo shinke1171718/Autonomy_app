b88ef384cc4920e35c36d20e436e51c05fdb204a
shinke <sinke.niinomi0506@icloud.com>
2023-07-12 20:39:38 +0900

    “Menuモデルを作成し、データベースのマイグレーションを実行しました。”

--- db/schema.rb
+++ db/schema.rb
@@ -10,9 +10,20 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_06_28_094149) do
+ActiveRecord::Schema[7.0].define(version: 2023_07_12_112346) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "menus", force: :cascade do |t|
+    t.string "menu_name", default: "", null: false
+    t.text "menu_contents", default: "", null: false
+    t.string "contents", default: "", null: false
+    t.boolean "original_menu", default: false, null: false
+    t.text "image_meta_data", default: "", null: false
+    t.string "image", default: "", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "name", default: "", null: false
