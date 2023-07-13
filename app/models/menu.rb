a413290236222fa6c808c66652650f8e7fb42a87
shinke <sinke.niinomi0506@icloud.com>
2023-07-13 13:27:10 +0900

    “MenuモデルとIngredientモデルの関連付けを設定”

--- app/models/menu.rb
+++ app/models/menu.rb
@@ -2,4 +2,5 @@ class Menu < ApplicationRecord
   has_many :menu_users, dependent: :destroy
   has_many :users, through: :menu_users
   has_one_attached :image
+  has_many :ingredients, dependent: :destroy
 end
