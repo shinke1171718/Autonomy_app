999958d447a584a85f462bdf5b1af5b7d6a59083
shinke <sinke.niinomi0506@icloud.com>
2023-07-13 13:49:17 +0900

    “「User」と「Menu」と「Ingredient」モデルをルーチングを設定”

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,10 @@
 Rails.application.routes.draw do
   root to: 'users#index'
+
   get 'users/:id/my_page', to: 'users#my_page', as: :user_my_page
+
   devise_for :users
+
   devise_scope :user do
     get 'users/registration', to: 'custom_registrations#new', as: :new_user_custom_registration
     post 'users/registration', to: 'custom_registrations#create', as: :user_custom_registration
@@ -10,5 +13,12 @@ Rails.application.routes.draw do
     get 'users/sessions', to: 'custom_sessions#destroy', as: :destroy_user_custom_session
     get 'registrations/edit', to: 'custom_registrations#edit', as: :edit_user_custom_registration
     patch 'registrations/update', to: 'custom_registrations#update', as: :update_user_custom_registration
+
+    resources :users do
+      resources :menus do
+        resources :ingredients
+      end
+    end
   end
+
 end
