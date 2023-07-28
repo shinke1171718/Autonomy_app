34605528b36337e060790694a11f6ba9910941af
shinke <sinke.niinomi0506@icloud.com>
2023-07-28 12:45:25 +0900

    “献立情報の登録と表示機能を追加しました。食材の登録は未実装です。”

--- app/controllers/menus_controller.rb
+++ app/controllers/menus_controller.rb
@@ -1,2 +1,47 @@
 class MenusController < ApplicationController
+
+  def index
+    @original_menus = Menu.where(original_menu: true)
+    @default_menus = Menu.where(original_menu: false)
+  end
+
+  def new
+    @menu = Menu.new
+  end
+
+  def create
+    menu = Menu.new(menu_params)
+    if menu.menu_name.blank?
+      flash[:notice] = "献立名を登録してください。"
+      render 'new'
+      return
+    end
+
+    if menu.menu_contents.blank?
+      flash[:notice] = "献立内容を登録してください。"
+      render 'new'
+      return
+    end
+
+    if menu.contents.blank?
+      flash[:notice] = "献立の詳細を登録してください。"
+      render 'new'
+      return
+    end
+
+    if menu.image.attached? == false
+      menu_image = File.open('app/assets/images/default-menu-icon.png')
+      menu.image.attach(io: menu_image, filename: 'default-menu-icon.jpg')
+    end
+
+    menu.original_menu = true
+    menu.save
+  end
+
+
+  private
+
+  def menu_params
+    params.require(:menu).permit(:menu_name, :menu_contents, :contents, :original_menu, :image, :image_meta_data)
+  end
 end
