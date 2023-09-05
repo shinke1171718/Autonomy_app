0cf1a568ed7ac57ba8df7518e6f006396718d415
shinke <sinke.niinomi0506@icloud.com>
2023-09-05 15:44:59 +0900

    “献立登録フォームのviewコードを別ブランチでプルリクするため一時的にコミット”

--- app/controllers/menus_controller.rb
+++ app/controllers/menus_controller.rb
@@ -1,7 +1,4 @@
 class MenusController < ApplicationController
-  before_action :delete_menu_session, only: [:index]
-  before_action :delete_flash, only: [:new_confirm]
-
 
   def index
     menu_ids = UserMenu.where(user_id: current_user.id).pluck(:menu_id)
@@ -16,164 +13,80 @@ class MenusController < ApplicationController
 
 
   def new_confirm
-    if session[:menu].nil? && session[:temp_ingredients].nil?
-      session[:menu] = menu_params
-      session[:temp_ingredients] = []
-    end
+    flash[:notice] = nil
 
-    # renderを行なった際に「フォームの属性キー」を付与するため@menuにデータを格納
-    @menu = Menu.new(menu_params)
-    # menuデータを更新
-    update_session_menu_data(menu_params, menu_image_params)
+    validate_menu_params(menu_params)
+    menu = Menu.new(menu_params)
 
-    # 追加ボタンを押された処理
-    if params[:ingredient] == "追加"
+    binding.pry
+    # ingredients_params = params[:ingredient]
+    # unless ingredients_params.nil?
+    #   ingredients_params = params.require(:ingredient)
+    #   # あとで修正が必要
+    #   # ingredients = build_ingredients_from_params(ingredients_params)
+    #   validate_ingredients_params(ingredients)
+    # end
 
-      new_ingredient = ingredient_params
-      temp_ingredients = session[:temp_ingredients]
-      validate_new_ingredient(temp_ingredients, new_ingredient)
-
-      if flash[:notice].present?
-        redirect_to new_user_menu_path(current_user)
-        return
-      end
-
-      if temp_ingredients.nil?
-        temp_ingredients = new_ingredient
-      else
-        temp_ingredients << new_ingredient
-      end
-
-      flash[:notice] = '食材が追加されました'
-      session[:temp_ingredients] = temp_ingredients
-
-      redirect_to new_user_menu_path(current_user)
+    if flash[:notice].present?
+      # redirect_to new_user_menu_path
+      render 'new', status: :unprocessable_entity
       return
     end
 
-
-    # 削除ボタンを押された処理
-    if params[:delete] == "✖️"
-      ingredient_name = params[:ingredient][:name]
-      session[:temp_ingredients].reject! { |ingredient| ingredient["name"] == ingredient_name }
-      set_flash_notice("食材を削除しました。")
-      redirect_to new_user_menu_path(current_user)
-    end
-
-
-    # 次へボタンを押された処理
-    if params[:create] == "次へ"
-
-      validate_menu(menu_params, menu_image_params)
-
-      if flash[:notice].present?
-        render 'new'
-        return
-      end
-
-      binding.pry
-      render 'confirm'
-    end
-
+    render 'confirm', status: :unprocessable_entity
   end
 
 
-  def create
-    # extract_image_data_from_session(session[:menu_image])
-    binding.pry
-    save_menu_with_image(session[:menu], session[:menu_image])
-    binding.pry
-    UserMenu.create(user: current_user, menu: menu)
-    create_ingredients(menu, session[:temp_ingredients])
-    binding.pry
-    redirect_to user_menus_path(current_user)
-  end
 
-
-  # def edit
-  #   @menu = Menu.find(params[:id])
-
-  #   # 既存データをtemp_ingredientsに移行
-  #   move_ingredients_to_temp(@menu)
-  #   @temp_ingredients = get_t_ingredients_for_user(current_user)
-
-  #   save_menu_data_to_session(menu)
-  # end
-
-
-  # def edit_confirm
-  #   @menu = session[:menu_data]['menu']
-  #   @temp_ingredients = get_temp_ingredients_for_user(current_user)
-
-  #   validate_menu(menu)
-  #   render_if_flash_present('edit')
-  # end
-
-
-  # def update
-  #   menu = Menu.find(params[:id])
-  #   validate_and_render_or_set_original(menu)
-  #   render_or_update_menu(menu, 'edit')
-
-  #   assign_and_save_temp_ingredients(current_user, menu)
-
-  #   redirect_to_confirm_user_menu(current_user, menu)
-  # end
-
-
-  # def destroy_related_data
-  #   menu = Menu.find(params[:id])
-  #   Ingredient.where(menu_id: menu.id).destroy_all
-  #   UserMenu.where(menu_id: menu.id).destroy_all
-  #   menu.destroy
-  #   reset_menu_processing
-  #   redirect_to user_menus_path(current_user)
-  # end
-
-
-  def reset_session
-    reset_menu_processing
-    redirect_to user_menus_path(current_user)
-  end
 
 
   private
 
-  def ingredient_params
-    ingredient_params = params.require(:menu).require(:ingredients).permit(:name, :quantity, :unit)
-  end
-
   def menu_params
-    # menu_params = params.require(:menu).permit(:menu_name, :menu_contents, :contents, :original_menu, :image, :image_meta_data)
-    menu_params = params.require(:menu).permit(:menu_name, :menu_contents, :contents)
+    menu_params = params.require(:menu).permit(:menu_name, :menu_contents, :contents, :image, :image_meta_data)
   end
 
-  def menu_image_params
-    menu_image_params = params.require(:menu).permit(:image, :image_meta_data)
+  def build_ingredients_from_params(ingredients_params)
+    ingredients = []
+
+    ingredients_params.each do |ingredient_param|
+      ingredient = Ingredient.new(
+        name: ingredient_param[:name],
+        quantity: ingredient_param[:quantity],
+        unit: ingredient_param[:unit]
+      )
+      ingredients << ingredient
+    end
+
+  end
+
+  def validate_ingredients_params(ingredients)
+    ingredients.each_with_index do |ingredient|
+
+      if ingredient[:name].blank? || ingredient[:quantity].blank? ||
+        (ingredient[:quantity].blank?) ||
+        (ingredient[:name].length > 15) ||
+        (ingredient[:unit].blank?)
+        set_flash_notice("入力内容に不具合があります。")
+      end
+
+    end
+  end
+
+  def validate_menu_params(menu_params)
+    if menu_params[:image].blank?
+      default_image = Rails.root.join("app/assets/images/default-menu-icon.png")
+      menu_params[:image] = { io: File.open(default_image), filename: "default-menu-icon.png" }
+    end
+
+    if menu_params["menu_name"].blank? || menu_params["menu_contents"].blank? || menu_params["contents"].blank?
+      set_flash_notice("入力内容に不具合があります。")
+    end
+
   end
 
 
-  def delete_menu_session
-    session[:temp_ingredients] = nil
-    session[:menu] = nil
-    session[:menu_image] = nil
-  end
 
-  def delete_flash
-    flash[:notice] = nil
-  end
-
-  # def save_menu_data_to_session(menu)
-  #   session[:menu_data] = {
-  #     menu: menu.attributes
-  #   }
-  # end
-
-  def attach_default_menu_image(menu)
-    menu_image = File.open('app/assets/images/default-menu-icon.png')
-    # menu[:image].attach(io: menu_image, filename: 'default-menu-icon.jpg')
-    session[:menu_image] = menu_image
-  end
 
   def validate_menu(menu, menu_image)
     if menu["menu_name"].blank?
@@ -186,14 +99,6 @@ class MenusController < ApplicationController
       attach_default_menu_image(menu)
     end
   end
-
-  # def validate_temp_ingredient(existing_date, new_date)
-  #   matching_ingredient = existing_date.find { |ingredient| ingredient["name"] == new_date["name"] }
-
-  #   unless matching_ingredient.nil?
-  #     flash[:notice] = '同じ名前の食材がすでに存在します。'
-  #   end
-  # end
 
   def validate_new_ingredient(existing_date, new_date)
 
@@ -214,11 +119,6 @@ class MenusController < ApplicationController
     end
   end
 
-  # def validate_and_set_original(menu)
-  #   validate_menu(menu)
-  #   menu.original_menu = true
-  # end
-
   def redirect_to_if_flash_present(path)
     if flash[:notice].present?
       redirect_to path
@@ -231,14 +131,6 @@ class MenusController < ApplicationController
     end
   end
 
-  # def render_or_update_menu(menu, view)
-  #   if flash[:notice].present?
-  #     render view
-  #     return
-  #   else
-  #     menu.update
-  #   end
-  # end
 
   # def assign_and_save_temp_ingredients(user, menu)
   #   ingredients = TempIngredient.where(user_id: user.id)
@@ -291,17 +183,6 @@ class MenusController < ApplicationController
       ingredient.save
     end
   end
-
-  # def extract_image_data_from_session(menu_image)
-  #   if menu_image.present?
-  #     tempfile_path = menu_image["tempfile"]
-  #     binding.pry
-  #     @image_data_view = File.read(tempfile_path)
-  #     @image_data = File.open(tempfile_path, "rb")
-  #     @content_type = menu_image["content_type"]
-  #     @original_filename = menu_image["original_filename"]
-  #   end
-  # end
 
   def update_session_menu_data(menu_params, menu_image_params)
     session[:menu] = menu_params
--- app/javascript/form-add-remove.js
+++ app/javascript/form-add-remove.js
@@ -4,36 +4,41 @@ window.$ = jquery
 $(function() {
   var maxCount = 9;
   var formPlusButton = $('#ingredient-plus');
-  var registrationField = $('.custom-ingredient-fields');
   var formContainer = $('.ingredient-form-container');
   var formCount = 0;
 
   formPlusButton.on('click', function() {
-    // // maxCount以内の場合
     if (formCount < maxCount) {
       var newForm = $('<div class="custom-ingredient-fields">\
       <div id="ingredient-delete">\
         <a class="ingredient-delete-actions">❌</a>\
       </div>\
-      <input type="text" name="ingredient_name[]" autofocus="true" autocomplete="name" placeholder=" 食材名 (上限15文字)" maxlength="15" class="ingredient-name">\
-      <input type="text" name="ingredient_quantity[]" autofocus="true" autocomplete="quantity" placeholder=" 数量" maxlength="4" class="ingredient-quantity">\
-      <select name="ingredient_unit[]" class="ingredient-unit">\
+      <input type="text" name="ingredient[][name]" autofocus="true" autocomplete="name" placeholder=" 食材名 (上限15文字)" maxlength="15" class="ingredient-name">\
+      <input type="text" name="ingredient[][quantity]" autofocus="true" autocomplete="quantity" placeholder=" 数量" maxlength="4" class="ingredient-quantity">\
+      <select name="ingredient[][unit]" class="ingredient-unit">\
         <option value="" selected>単位</option>\
         <option value="g">g</option>\
         <option value="kg">kg</option>\
         <option value="ml">ml</option>\
-        <!-- 他のオプションも同様に記述 -->\
+        <option value="L">L</option>\
+        <option value="個">個</option>\
+        <option value="枚">枚</option>\
+        <option value="匹">匹</option>\
+        <option value="切れ">切れ</option>\
+        <option value="杯">杯</option>\
+        <option value="缶">缶</option>\
+        <option value="本">本</option>\
+        <option value="袋">袋</option>\
       </select>\
     </div>');
 
 
 
-      var newFormInputs = newForm.find('input, select');
+      var newFormInputs = newForm.find('input[name="ingredient[][name]"], input[name="ingredient[][quantity]"], select[name="ingredient[][unit]"]');
       newFormInputs.each(function() {
         $(this).val('');
       });
 
-      // $('.custom-ingredient-fields').last().after(newForm);
       formContainer.append(newForm);
       formCount++;
     }
