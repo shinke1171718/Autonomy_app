3d2dab5c1a9d4e4b66d009efaef92a7ac54b66d8
shinke <sinke.niinomi0506@icloud.com>
2023-10-06 15:15:28 +0900

    “フロントエンドでの名前重複バリデーションを実装”

--- app/javascript/menu_validation.js
+++ app/javascript/menu_validation.js
@@ -1,46 +1,69 @@
-document.addEventListener('turbo:load', function(event) {
-  var form = document.getElementById('menu_form');
+document.addEventListener("turbo:load", function(event) {
+  var form = document.getElementById("menu_form");
+  var minForm = 0;
+  var maxForm = 14;
 
   form.addEventListener('submit', function(event) {
-
-    var menu_name = form.elements['menu_name'];
-    var menu_contents = form.elements['menu_contents'];
-    var contents = form.elements['contents'];
-    var errorMessage_1 = document.getElementById('menu-error_name');
-    var errorMessage_2 = document.getElementById('menu-error-contents-1');
-    var errorMessage_3 = document.getElementById('menu-error-contents-2');
+    var menu_name = form.elements["menu_name"];
+    var menu_contents = form.elements["menu_contents"];
+    var contents = form.elements["contents"];
+    var errorMessage_name = document.getElementById("menu-error_name");
+    var errorMessage_contents = document.getElementById("menu-error-contents-1");
+    var errorMessage_contents_2 = document.getElementById("menu-error-contents-2");
     var inputName = document.querySelector(".name-registration-field input");
     var inputContent = document.querySelector(".menu-registration-field input");
     var inputText = document.querySelector("textarea");
 
-    // menu_nameがnullの場合にエラーメッセージを代入
-    validateAndHighlightInput(menu_name, errorMessage_1, inputName, 20, event)
-    // menu_contentsがnullの場合にエラーメッセージを代入
-    validateAndHighlightInput(menu_contents, errorMessage_2, inputContent, 20, event)
-    // contentsがnullの場合にエラーメッセージを代入
-    validateAndHighlightInput(contents, errorMessage_3, inputText, 700, event)
+    validateAndHighlightInput(menu_name, errorMessage_name, inputName, event)
+    validateAndHighlightInput(menu_contents, errorMessage_contents, inputContent, event)
+    validateAndHighlightInput(contents, errorMessage_contents_2, inputText, event)
+  });
 
+
+
+  form.addEventListener('submit', function(event) {
+    for (var i = minForm; i < maxForm; i++) {
+      validateInput("ingredient_name[" + i + "]");
+      validateInput("ingredient_quantity[" + i + "]");
+      validateInput("menu_ingredients_[" + i + "]unit");
+    }
   });
 
 
   form.addEventListener('submit', function(event) {
-    var minForm = 0;
-    var maxForm = 14;
-    var errorMessage_ingredient = document.getElementById('ingredients-error');
+    var names = [];
+    var duplication_error = document.getElementById("ingredients-name-duplication-error");
+    function isNameValid(name) {
+      return name.trim() !== "" && !names.includes(name);
+    }
 
     for (var i = minForm; i < maxForm; i++) {
-      validateInput("ingredient_name[" + i + "]", 15, errorMessage_ingredient);
-      validateInput("ingredient_quantity[" + i + "]", 3, errorMessage_ingredient);
-      validateInput("menu_ingredients_[" + i + "]unit", 3, errorMessage_ingredient);
+      var ingredient_name = document.getElementById("ingredient_name[" + i + "]");
+      if (ingredient_name.value.trim() === "") {
+        continue;
+      }
+
+      var name = ingredient_name.value.trim();
+      if (isNameValid(name)) {
+        names.push(name);
+        ingredient_name.style.backgroundColor = '';
+      } else {
+        ingredient_name.style.backgroundColor = "rgb(255, 184, 184)";
+        duplication_error.textContent = "⚠️重複登録された食材があります。";
+      }
     }
+
+
   });
 });
 
 
-function validateAndHighlightInput(element, errorMessage, inputElement, maxLength, event) {
-  if (element.value === "" || element.value.length > maxLength) {
+function validateAndHighlightInput(element ,sub_errorMessage, inputElement, event) {
+  var menu_main_errorMessage = document.getElementById("main-menu-error");
+  if (element.value === "" ) {
     event.preventDefault();
-    errorMessage.textContent = "⚠️必須項目です。";
+    menu_main_errorMessage.textContent = "⚠️未入力があります。";
+    sub_errorMessage.textContent = "⚠️必須";
     inputElement.style.backgroundColor = "rgb(255, 184, 184)";
   } else {
     errorMessage.textContent = "";
@@ -49,15 +72,15 @@ function validateAndHighlightInput(element, errorMessage, inputElement, maxLengt
 }
 
 
-function validateInput(inputId, maxLength, errorMessageElement) {
+function validateInput(inputId) {
+  var errorMessage_ingredient = document.getElementById('ingredients-error');
   var input = document.getElementById(inputId);
-  if (input && input.value === "" || element.value.length > maxLength) {
+  if (input && input.value === "" ) {
     event.preventDefault();
     input.style.backgroundColor = "rgb(255, 184, 184)";
-    errorMessageElement.textContent = "⚠️未入力があります。";
+    errorMessage_ingredient.textContent = "⚠️未入力があります。";
   } else if (input) {
     input.style.backgroundColor = "";
   }
 }
-
 
