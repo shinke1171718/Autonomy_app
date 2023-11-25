ae07e688cd389fb7910d845e1b2f75b8629c26ad
shinke <sinke.niinomi0506@icloud.com>
2023-11-25 23:19:10 +0900

    “無駄な条件分岐処理を削除しました。”

--- app/javascript/image_preview.js
+++ app/javascript/image_preview.js
@@ -3,25 +3,23 @@ createPreview()
 function createPreview() {
   var imageInput = document.getElementById('menu_image');
 
-  if (imageInput) {
-    imageInput.addEventListener('change', function(event) {
-      // ユーザーが選択したファイルを取得
-      let file = event.target.files[0];
-      // Base64エンコードされたデータURLおを作成して格納
-      let reader = new FileReader();
+  if (!imageInput) return;
 
-      reader.onload = function(e) {
-        let imagePreview = document.getElementById('image-preview');
+  imageInput.addEventListener('change', function(event) {
+    // ユーザーが選択したファイルを取得
+    let file = event.target.files[0];
+    // Base64エンコードされたデータURLを作成して格納
+    let reader = new FileReader();
 
-        if (imagePreview) {
-          // Base64エンコードされたデータURLをsrc属性に代入
-          imagePreview.src = e.target.result;
-          imagePreview.style.display = 'block';
-        }
-      };
+    reader.onload = function(e) {
+      let imagePreview = document.getElementById('image-preview');
 
-      // 画像データを取得するための命令
-      reader.readAsDataURL(file);
-    });
-  }
+      // Base64エンコードされたデータURLをsrc属性に代入
+      imagePreview.src = e.target.result;
+      imagePreview.style.display = 'block';
+    };
+
+    // 画像データを取得するための命令
+    reader.readAsDataURL(file);
+  });
 }
