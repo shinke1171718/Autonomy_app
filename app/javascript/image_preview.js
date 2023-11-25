0647c7a36475d511d37693c91737c7db25c5af6c
shinke <sinke.niinomi0506@icloud.com>
2023-11-25 21:09:35 +0900

    “コードの説明をコメントアウトで設定“

--- app/javascript/image_preview.js
+++ app/javascript/image_preview.js
@@ -2,22 +2,25 @@ createPreview()
 
 function createPreview() {
   var imageInput = document.getElementById('menu_image');
-  console.log(imageInput);
 
   if (imageInput) {
     imageInput.addEventListener('change', function(event) {
+      // ユーザーが選択したファイルを取得
       let file = event.target.files[0];
+      // Base64エンコードされたデータURLおを作成して格納
       let reader = new FileReader();
 
       reader.onload = function(e) {
         let imagePreview = document.getElementById('image-preview');
 
         if (imagePreview) {
+          // Base64エンコードされたデータURLをsrc属性に代入
           imagePreview.src = e.target.result;
           imagePreview.style.display = 'block';
         }
       };
 
+      // 画像データを取得するための命令
       reader.readAsDataURL(file);
     });
   }
