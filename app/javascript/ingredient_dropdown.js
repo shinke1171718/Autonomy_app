e751553ab3c567579cb15f24aabd999fbef220af
shinke <sinke.niinomi0506@icloud.com>
2023-11-26 17:28:04 +0900

    “検索結果表示の不具合解消のためにsearchTimerを削除”

--- app/javascript/ingredient_dropdown.js
+++ app/javascript/ingredient_dropdown.js
@@ -1,71 +1,85 @@
+
+// 'click'イベントをingredient_formに追加し、クリックがあるたびに関数を実行
 ingredient_form.addEventListener("click", function(event) {
+  // 材料選択のドロップダウン要素を取得
   let ingredientList = document.getElementById(`ingredient-select`);
+  // ドロップダウンの背景要素を取得
   let dropdownBg = document.getElementById('dropdownBackground');
+  // 検索結果のタイトル要素を取得
   let searchResultsTitle = document.querySelector(".search-results-title");
   const clickedElement = event.target;
 
+  // クリックされた要素が「材料量の入力フィールド」「数量減少ボタン」であれば処理を終了
   if (clickedElement.classList.contains("ingredient-quantity")) return;
   if (clickedElement.classList.contains("form-count-down")) return;
 
+  // クリックされた要素が材料単位の選択肢であれば処理を行い終了
   if (clickedElement.classList.contains("ingredient-unit")) {
     handleIngredientUnitChange(clickedElement);
     return;
   }
 
+  // クリックされた要素に対応する材料名の要素を取得
   ingredientName = document.getElementById(clickedElement.id);
   if (!ingredientName) return;
+  // ドロップダウンを開く処理を実行
   openDropdown(ingredientList, dropdownBg, searchResultsTitle);
   event.stopPropagation();
-
-  let searchTimer;
+  // 検索入力フィールドを取得
   const searchInput = document.getElementById('ingredientSearchInput');
 
+  // searchInputの中にエンターで別のアクションを発生させないための処理
   searchInput.addEventListener("keydown", function(event) {
     if (event.key === "Enter") {
         event.preventDefault();
     }
   });
 
+  // 'input'イベントをsearchInputに追加し、入力があるたびに関数を実行
   searchInput.addEventListener("input", function(e) {
+    // 材料リストからすべてのリストアイテムを取得
     const ingredientItems = ingredientList.querySelectorAll('li');
-    // タイマーが既に設定されている場合はクリア
-    if (searchTimer) {
-      clearTimeout(searchTimer);
-    }
 
     e.preventDefault();
+    // 一致したアイテムを保持する配列を初期化
     matchedItems = [];
+    // 検索結果コンテナの内容を空にする
     searchResultsContainer.innerHTML = '';
+    // 入力された検索テキストを取得し、前後の空白を削除
     const searchText = searchInput.value.trim();
 
+    // 検索テキストが空の場合、検索結果をクリアして処理を終了
     if (searchText === '') {
       clearSearchResults(searchResultsTitle);
       return;
     }
 
+    // 各材料アイテムに対してループ処理
     ingredientItems.forEach(function(item) {
+      // アイテムのひらがなデータを取得
       const itemHiragana = item.getAttribute('data-hiragana');
+      // アイテムの漢字データを取得
       const itemValue = item.getAttribute('data-material-name');
 
-      // ひらがな前方一致
+      // 検索テキストでひらがなが前方一致する場合、アイテムを配列に追加
       if (itemHiragana.startsWith(searchText)) {
         matchedItems.push(item);
         return;
       }
 
-      // ひらがな部分一致
+      // 検索テキストでひらがなが部分一致する場合、アイテムを配列に追加
       if (itemHiragana.includes(searchText)) {
         matchedItems.push(item);
         return;
       }
 
-      // 漢字の前方一致
+      // 検索テキストで漢字が前方一致する場合、アイテムを配列に追加
       if (itemValue.startsWith(searchText)) {
         matchedItems.push(item);
         return;
       }
 
-      // 漢字の部分一致
+      // 検索テキストで漢字が部分一致する場合、アイテムを配列に追加
       if (itemValue.includes(searchText)) {
         matchedItems.push(item);
         return;
@@ -74,20 +88,19 @@ ingredient_form.addEventListener("click", function(event) {
 
     // ソートされたアイテムを結果として表示
     matchedItems.forEach(function(item) {
+      // アイテムのテキストを取得し、トリミング
       const itemText = item.textContent.trim();
       searchResultsTitle.style.display = "block";
+      // 新しいdiv要素を作成し、検索結果アイテムとして設定
       const resultDiv = document.createElement('div');
       resultDiv.textContent = itemText;
       resultDiv.classList.add('search-result-item');
+      // 作成したdivを検索結果コンテナに追加
       searchResultsContainer.appendChild(resultDiv);
     });
 
+    // 材料リストを表示
     ingredientList.style.display = "block";
-
-    // 3秒後に処理を終了
-    searchTimer = setTimeout(() => {
-      clearSearchResults(searchResultsTitle);
-    }, 3000);
   });
 
   // 食材を選んだらフォームに値をセットする
