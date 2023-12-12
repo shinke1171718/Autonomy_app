06ca778547052655edf79f14247b7c57078b7e93
Apple <apple@ShinkenoMacBook-4.local>
2023-12-12 22:42:39 +0900

    “買い物リストのviewをindex.html.erbで作成＋controllerを一部修正“

--- db/migrate/20231211081014_create_shopping_list_menus.rb
+++ db/migrate/20231211081014_create_shopping_list_menus.rb
@@ -3,6 +3,7 @@ class CreateShoppingListMenus < ActiveRecord::Migration[7.0]
     create_table :shopping_list_menus do |t|
       t.references :shopping_list, null: false, foreign_key: true
       t.references :menu, null: false, foreign_key: true
+      t.integer :menu_count
 
       t.timestamps
     end
