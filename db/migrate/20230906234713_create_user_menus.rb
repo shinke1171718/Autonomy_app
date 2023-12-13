f6d13a0d14f5db8b2b6de4ddb13c0be7eb7121de
Apple <apple@ShinkenoMacBook-4.local>
2023-12-13 15:50:37 +0900

    “開発者用のサンプル献立登録機能をサポートするためのマイグレーションファイルの更新“

--- db/migrate/20230906234713_create_user_menus.rb
+++ db/migrate/20230906234713_create_user_menus.rb
@@ -1,8 +1,8 @@
 class CreateUserMenus < ActiveRecord::Migration[7.0]
   def change
     create_table :user_menus do |t|
-      t.references :user,                null: false
-      t.references :menu,                null: false
+      t.references :user
+      t.references :menu, null: false
       t.timestamps
     end
   end
