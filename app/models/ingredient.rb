05e4a333bea7bbe9cc7d0dc7d5ea51b955844735
“shinke1171718” <sinke.niinomi0506@icloud.com>
2023-12-27 14:51:05 +0900

    “献立登録・更新画面の食材表示をユーザー入力通りに変更、quantityバリデーション修正“

--- app/models/ingredient.rb
+++ app/models/ingredient.rb
@@ -6,11 +6,14 @@ class Ingredient < ApplicationRecord
 
   validates :material_name, presence: true
   validates :material_id, presence: true, length: { maximum: 15 }
-  validates :quantity, presence: true, length: { maximum: 4 }, unless: :skip_quantity_validation?
+
+# 'quantity'は10桁まで許容。小数点含むためと、合算時の桁数考慮のため
+  validates :quantity, presence: true, length: { maximum: 10 }, unless: :skip_quantity_validation?
   validates :unit_id, presence: true
 
   private
 
+  # 特定の条件下で'quantity'バリデーションをスキップ
   def skip_quantity_validation?
     settings = YAML.load_file(Rails.root.join('config', 'settings.yml'))
     unit_id == settings.dig('ingredient', 'no_quantity_unit_id')
