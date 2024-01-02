daa36e5ab38b2c2d05ba99debc9d3c7a59231c9b
“shinke1171718” <sinke.niinomi0506@icloud.com>
2024-01-02 18:08:03 +0900

    “ユーザー情報とパスワード更新の分離及びバリデーション強化“

--- app/controllers/custom_registrations_controller.rb
+++ app/controllers/custom_registrations_controller.rb
@@ -38,38 +38,82 @@ class CustomRegistrationsController < ApplicationController
   def edit
   end
 
+  def edit_password
+  end
+
   def update
-    # パスワードがない場合
-    unless edit_user_params[:password].present? && edit_user_params[:password_confirmation].present?
-      current_user.update(user_params_without_password)
-      flash[:notice] = "ユーザー情報を更新（パスワード未更新）"
-      render :edit
+    # ユーザー情報の更新
+    if user_params.present?
+      update_user_info
       return
     end
 
-    # パスワードが一致しない場合
-    if current_user.valid_password?(edit_user_params[:current_password])
-      current_user.update(edit_user_params)
-      flash[:notice] = "ユーザー情報とパスワードを更新再度ログインをお願いします。"
-      redirect_to user_custom_session_path
-    else
-      flash[:notice] = "現在のパスワードが正しくありません。"
-      render :edit
+    # パスワード更新
+    # 未入力があるかチェック
+    if !password_params.keys.all? { |key| password_params[key].present? }
+      flash[:error] = "未入力がありました。"
+      redirect_to edit_password_user_custom_registration_path
+      return
     end
+
+    # 現在のパスワードがあっているかチェック
+    if !current_user.valid_password?(password_params[:current_password])
+      flash[:error] = "入力された「現在のパスワード」が正しくありません。"
+      redirect_to edit_password_user_custom_registration_path
+      return
+    end
+
+    # パスワードをアップデート
+    if current_user.update(password_params)
+      flash[:notice] = "パスワードを更新しました。再度ログインをお願いします。"
+      redirect_to edit_password_user_custom_registration_path
+      return
+
+    # パスワードの「形式誤り」と「新しいパスワードと再入力が一致しない」場合の処理
+    else
+      check_password_errors
+      redirect_to edit_password_user_custom_registration_path
+    end
+
   end
 
   private
 
+  def set_flash_error_messages
+    if current_user.errors.details[:name].any? { |error| error[:error] == :blank } ||
+       current_user.errors.details[:email].any? { |error| error[:error] == :blank }
+      flash[:error] = "未入力の項目がありました。"
+    else
+      flash[:error] = "ユーザー情報の更新に失敗しました。"
+    end
+  end
+
+  # ユーザー情報の更新を行うメソッド
+  def update_user_info
+    if current_user.update(user_params)
+      flash[:notice] = "ユーザー情報を更新しました。"
+      redirect_to edit_user_custom_registration_path
+    else
+      set_flash_error_messages
+      redirect_to edit_user_custom_registration_path
+    end
+  end
+
   def user_params
-    params.require(:user).permit(:name, :email, :password, :password_confirmation)
-  end
-
-  def edit_user_params
-    params.require(:user).permit(:name, :email, :password, :password_confirmation, :current_password)
-  end
-
-  def user_params_without_password
     params.require(:user).permit(:name, :email)
+  end
+
+  def password_params
+    params.require(:user).permit(:password, :password_confirmation, :current_password)
+  end
+
+  # パスワードの「形式誤り」か「新しいパスワードと再入力が一致しない」場合にエラーメッセージを設定する
+  def check_password_errors
+    if current_user.errors.added?(:password_confirmation, "doesn't match Password")
+      flash[:error] = "新しいパスワード(確認)は新しいパスワードと一致しません。"
+    elsif current_user.errors.added?(:password, "is invalid")
+      flash[:error] = "パスワードには英字と数字を両方含む必要があります。"
+    end
   end
 
 end
