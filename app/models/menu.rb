7896d2733f69a2e4dbe10d1deccc7b38839c366b
“shinke1171718” <sinke.niinomi0506@icloud.com>
2024-01-29 01:20:28 +0900

    “menuの削除機能と確認ダイヤログの実装“

--- app/models/menu.rb
+++ app/models/menu.rb
@@ -1,5 +1,5 @@
 class Menu < ApplicationRecord
-  has_many :menu_users, dependent: :destroy
+  has_many :user_menu, dependent: :destroy
   has_many :users, through: :menu_users
   has_one_attached :image
   has_many :menu_ingredients, dependent: :destroy
