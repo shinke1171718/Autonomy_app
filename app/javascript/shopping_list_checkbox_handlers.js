35b5e032f46e10b1c69fa2e7966316bd73ae9e07
“shinke1171718” <sinke.niinomi0506@icloud.com>
2024-02-06 01:18:28 +0900

    “買い物リストのチェックボックスで要素クリックでもチェック可能に修正“

--- app/javascript/shopping_list_checkbox_handlers.js
+++ app/javascript/shopping_list_checkbox_handlers.js
@@ -1,5 +1,38 @@
+setupCheckboxEventListeners();
+setupIngredientItemClickEvents();
 
-setupCheckboxEventListeners()
+function setupCheckboxEventListeners() {
+  // すべての '.custom-checkbox' 要素を選択し、それぞれにリスナーを追加
+  document.querySelectorAll('.custom-checkbox').forEach(function(checkbox) {
+    checkbox.addEventListener('change', handleCheckboxChange);
+  });
+}
+
+function setupIngredientItemClickEvents() {
+  // すべての '.ingredient-item' 要素を選択し、それぞれにリスナーを追加
+  document.querySelectorAll('.ingredient-item').forEach(function(item) {
+    item.addEventListener('click', handleIngredientItemClick);
+  });
+}
+
+// ingredient-item がクリックされたときの処理
+function handleIngredientItemClick(e) {
+  // クリックされた要素から 'custom-checkbox' クラスを持つ要素を検索
+  let checkbox = e.currentTarget.querySelector('.custom-checkbox');
+  // チェックボックスの状態をトグル（切り替え）
+  checkbox.checked = !checkbox.checked;
+
+  // チェックボックスの変更イベントを手動で発火させる
+  triggerCheckboxChangeEvent(checkbox);
+}
+
+// チェックボックスの状態を変更するためのメソッド
+function triggerCheckboxChangeEvent(checkbox) {
+  // 'change' イベントを作成
+  const event = new Event('change');
+  // 作成したイベントを発生させる
+  checkbox.dispatchEvent(event);
+}
 
 // チェックボックスのイベントを設定する関数
 function setupCheckboxEventListeners() {
