d617073a34a632291fc74947efdd8c46a7ed7b0a
“shinke1171718” <sinke.niinomi0506@icloud.com>
2024-02-11 23:13:47 +0900

    “作り方の工程保存機能とmenu_idなしのバリデーションを実装“

--- app/models/ingredient.rb
+++ app/models/ingredient.rb
@@ -4,16 +4,15 @@ class Ingredient < ApplicationRecord
   belongs_to :material
   belongs_to :unit
 
-  validates :material_name, presence: { message: '登録中に予期せぬエラーが発生しました。' }
-  validates :material_id, presence: { message: '登録中に予期せぬエラーが発生しました。' }
+  common_error_message = '登録中に予期せぬエラーが発生しました。'
 
+  validates :material_name, presence: { message: common_error_message }
+  validates :material_id, presence: { message: common_error_message }
   # 'quantity'は10桁まで許容。小数点含むためと、合算時の桁数考慮のため
-  validates :quantity, presence: { message: '登録中に予期せぬエラーが発生しました。' }, length: { maximum: 10, too_long: '登録中に予期せぬエラーが発生しました。' }, unless: :skip_quantity_validation?
-
-  validates :unit_id, presence: { message: '登録中に予期せぬエラーが発生しました。' }
+  validates :quantity, presence: { message: common_error_message }, length: { maximum: 10, too_long: common_error_message }, unless: :skip_quantity_validation?
+  validates :unit_id, presence: { message: common_error_message }
 
   private
-
   # 特定の条件下で'quantity'バリデーションをスキップ
   def skip_quantity_validation?
     settings = YAML.load_file(Rails.root.join('config', 'settings.yml'))
--- app/models/recipe_step.rb
+++ app/models/recipe_step.rb
@@ -1,4 +1,24 @@
 class RecipeStep < ApplicationRecord
-  belongs_to :menu
+  belongs_to :menu, optional: true
   belongs_to :recipe_step_category
+
+  # 全てのバリデーションエラーメッセージを統一
+  common_error_message = '登録中に予期せぬエラーが発生しました。'
+
+  validates :recipe_step_category_id, presence: { message: common_error_message }
+  validates :description, presence: { message: common_error_message }, length: { maximum: 60, message: common_error_message }
+  # コールバックを使用してstep_orderを自動設定
+  before_validation :set_step_order, on: :create
+
+  private
+
+  def set_step_order
+    settings = YAML.load_file(Rails.root.join('config', 'settings.yml'))
+    initial_step_order = settings.dig('recipe_step', 'initial_step_order')
+    step_order_increment = settings.dig('recipe_step', 'step_order_increment')
+
+    # 現在のmenu_idに属する最後のstep_orderを取得し、1を加算する
+    # 現在のメニューに属するレシピステップがない場合は1を設定
+    self.step_order = (RecipeStep.where(menu_id: menu_id).maximum(:step_order) || initial_step_order) + step_order_increment
+  end
 end
